6946dc2eeb063d431dca303a65e0ece0f7dea369
Bruno Gonzales <holabrunogonzales@gmail.com>
2021-08-26 09:54:00 -0500

    load fonts

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState } from "react";
+import * as Font from "expo-font";
 import Home from "./screens/home";
+import AppLoading from "expo-app-loading";
+
+const getFonts = () =>
+  Font.loadAsync({
+    "nunito-regular": require("./assets/fonts/Nunito-Regular.ttf"),
+    "nunito-bold": require("./assets/fonts/Nunito-Bold.ttf"),
+  });
 
 export default function App() {
-  return <Home />;
-}
+  const [fontsLoaded, setFontsLoaded] = useState(false);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  if (fontsLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+        onError={() => console.log("Error!")}
+      />
+    );
+  }
+}
